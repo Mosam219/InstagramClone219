61a4d08351edda2b2028b3f516536b79c3575b95
Mosam <mosampatel78@gmail.com>
2021-11-26 10:59:25 +0530

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import Navbar from './components/navbar'
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Navbar />
   </React.StrictMode>,
   document.getElementById('root')
 );
