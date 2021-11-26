ed8df1e1ddf84beeabb3b200009f97ddefc5975e
Mosam <mosampatel78@gmail.com>
2021-11-26 10:33:18 +0530

    make it better

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5000
 const { MONGOURI } = require('./config/keys')
-
+import path from 'path'
 mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -21,9 +21,9 @@ app.use(express.json());
 app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
 app.use(require('./routes/user'))
-
+__dirname = path.resolve();
 if (process.env.NODE_ENV == "production") {
-    app.use(express.static('client/build'))
+    app.use(express.static(path.join(__dirname, "/client/build")))
     const path = require('path')
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
