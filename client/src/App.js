8455d8c43bddba44d1312600e967e1a4247e1410
Mosam <mosampatel78@gmail.com>
2021-11-29 19:02:38 +0530

    commit1

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,12 @@ const Routing = () => {
   const history = useHistory()
   const { state, dispatch } = useContext(UserContext)
   useEffect(() => {
-    history.push('/login')
+    const user = JSON.parse(localStorage.getItem("user"))
+    if (user) {
+      dispatch({ type: "USER", payload: user })
+    } else {
+      history.push('/login')
+    }
   }, [])
   return (
     <Switch>
