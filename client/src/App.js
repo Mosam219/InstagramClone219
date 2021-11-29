3411f9e6b8e2ebcac78c6454269796fe30eb2ed8
Mosam <mosampatel78@gmail.com>
2021-11-29 19:23:28 +0530

    commit1

--- client/src/App.js
+++ client/src/App.js
@@ -14,16 +14,6 @@ import { useEffect } from 'react/cjs/react.development'
 export const UserContext = createContext()
 
 const Routing = () => {
-  // const history = useHistory()
-  // const { state, dispatch } = useContext(UserContext)
-  // useEffect(() => {
-  //   const user = JSON.parse(localStorage.getItem("user"))
-  //   if (user) {
-  //     dispatch({ type: "USER", payload: user })
-  //   } else {
-  //     history.push('/login')
-  //   }
-  // }, [])
   return (
     <Switch>
       <Route exact path="/" component={Home}><Home /></Route>
