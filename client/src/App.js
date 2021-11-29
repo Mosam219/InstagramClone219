e45fd947985f3763266974cf290cef486ce845f3
Mosam <mosampatel78@gmail.com>
2021-11-29 11:41:42 +0530

    commit1

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ const Routing = () => {
   }, [])
   return (
     <Switch>
-      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/" component={Home}><Home /></Route>
       <Route exact path="/profile" component={Profile}><Profile /></Route>
       <Route path="/signup" component={SignUp}><SignUp /></Route>
       <Route path="/login" component={Login}><Login /></Route>
@@ -41,6 +41,18 @@ const Routing = () => {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
+  const history = useHistory()
+  // const { state, dispatch } = useContext(UserContext)
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"))
+
+    if (user) {
+      dispatch({ type: "USER", payload: user })
+    }
+    else {
+      history.push('/login')
+    }
+  }, []);
   return (
     <UserContext.Provider value={{ state, dispatch }}>
       <BrowserRouter>
